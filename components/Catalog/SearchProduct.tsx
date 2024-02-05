f6fa3cadb9195f00ca24731333a1cf42dd16fbe3
Kevin Cioch <kevin.cioch@gmail.com>
2024-02-05 03:47:12 +0100

    Added ContentContainer for searchbar

--- components/Catalog/SearchProduct.tsx
+++ components/Catalog/SearchProduct.tsx
@@ -10,6 +10,7 @@ import React, {
   useRef,
   useState,
 } from "react";
+import ContentContainer from "../UI/Container/ContentContainer";
 
 const SearchProduct = () => {
   const showModal =
@@ -50,44 +51,46 @@ const SearchProduct = () => {
       tabIndex={-1}
     >
       {showModal && (
-        <form className="flex items-center" onSubmit={handleSearch}>
-          <label htmlFor="search" className="sr-only">
-            Suche
-          </label>
-          <div className="relative w-full">
-            <input
-              type="text"
-              id="search"
-              className="block py-2.5 px-0 w-full text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-800"
-              placeholder="Suche nach Produkten"
-              value={searchTxt}
-              onChange={handleInputChange}
-              tabIndex={1}
-              ref={inputRef}
-            />
-          </div>
-          <button
-            type="submit"
-            className="p-2.5 ms-2 text-sm font-medium text-green-800 bg-transparent rounded-lg hover:text-green-700 focus:ring-4 focus:outline-none focus:ring-green-600"
-          >
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+        <ContentContainer>
+          <form className="flex items-center" onSubmit={handleSearch}>
+            <label htmlFor="search" className="sr-only">
+              Suche
+            </label>
+            <div className="relative w-full">
+              <input
+                type="text"
+                id="search"
+                className="block py-2.5 px-0 w-full text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-800"
+                placeholder="Suche nach Produkten"
+                value={searchTxt}
+                onChange={handleInputChange}
+                tabIndex={1}
+                ref={inputRef}
               />
-            </svg>
-            <span className="sr-only">Suche</span>
-          </button>
-        </form>
+            </div>
+            <button
+              type="submit"
+              className="p-2.5 ms-2 text-sm font-medium text-green-800 bg-transparent rounded-lg hover:text-green-700 focus:ring-4 focus:outline-none focus:ring-green-600"
+            >
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              <span className="sr-only">Suche</span>
+            </button>
+          </form>
+        </ContentContainer>
       )}
     </div>
   );
